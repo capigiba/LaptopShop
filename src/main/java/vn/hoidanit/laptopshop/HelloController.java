52ab60ee4ddf8878e4ca5e390a26cd481efe4d41
Gia Bao <21127017@student.hcmus.edu.vn>
2024-07-10 10:53:00 +0700

    add router user and admin

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from Spring Boot!";
+        return "Hello World from Giabao!";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "Only user!";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Only admin here!";
     }
 }
