4965997759186276ba9d0e02a6190aee84978f20
Võ Nguyên Gia Bảo <21127017@student.hcmus.edu.vn>
2024-07-11 10:24:45 +0700

    JSTL

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import vn.hoidanit.laptopshop.service.UserService;
 
@@ -14,8 +15,10 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        model.addAttribute("Capybara", test);
+        model.addAttribute("java", "javakhoquahuhu");
         return "hello12";
     }
 
