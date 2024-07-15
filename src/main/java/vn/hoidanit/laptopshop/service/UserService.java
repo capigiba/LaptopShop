d93a2d06e201faa3b6ab5522221ec156be19a370
Võ Nguyên Gia Bảo <21127017@student.hcmus.edu.vn>
2024-07-15 22:27:37 +0700

    view specific user

--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -23,6 +23,10 @@ public class UserService {
         return this.userRepository.findByEmail(email);
     }
 
+    public User getUserById(long id) {
+        return this.userRepository.findById(id);
+    }
+
     public User handleSaveUser(User user) {
         User capigiba = this.userRepository.save(user);
         System.out.println(capigiba);
