b364d61e7c0293192e6f22bbc806703dc55aa86a
Võ Nguyên Gia Bảo <21127017@student.hcmus.edu.vn>
2024-07-19 21:02:28 +0700

    update user information

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -58,6 +59,26 @@ public class UserController {
     @RequestMapping(value = "admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User capigiba) {
         this.userService.handleSaveUser(capigiba);
+        return "redirect:/admin/user";
+    }
+
+    @RequestMapping(value = "/admin/user/update/{id}", method = RequestMethod.GET)
+    public String getUpdateUserPage(Model model, @PathVariable long id) {
+        User currentUser = this.userService.getUserById(id);
+        model.addAttribute("newUser", currentUser);
+        return "admin/user/update";
+    }
+
+    @PostMapping("/admin/user/update")
+    public String postUpdateUser(Model model, @ModelAttribute("newUser") User capigiba) {
+        User currentUser = this.userService.getUserById(capigiba.getId());
+        if (currentUser != null) {
+            currentUser.setAddress(capigiba.getAddress());
+            currentUser.setFullname(capigiba.getFullname());
+            currentUser.setPhone(capigiba.getPhone());
+
+            this.userService.handleSaveUser(capigiba);
+        }
         return "redirect:/admin/user";
     }
 
--- src/main/webapp/WEB-INF/view/admin/user/table-user.jsp
+++ src/main/webapp/WEB-INF/view/admin/user/table-user.jsp
@@ -46,7 +46,8 @@
                                             <td>${user.fullname}</td>
                                             <td>
                                                 <a href="/admin/user/${user.id}" class="btn btn-success">View</a>
-                                                <a class="btn btn-warning mx-2">Update</a>
+                                                <a href="/admin/user/update/${user.id}"
+                                                    class="btn btn-warning mx-2">Update</a>
                                                 <a class="btn btn-danger">Delete</a>
                                             </td>
                                         </tr>
