fada8ae535c7edd4bb8d6282ae472aa9fc667b4f
Võ Nguyên Gia Bảo <giabao@GB.local>
2024-08-07 21:38:47 +0700

    upload file image

--- src/main/java/vn/capigiba/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/capigiba/laptopshop/controller/admin/UserController.java
@@ -1,5 +1,9 @@
 package vn.capigiba.laptopshop.controller.admin;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -12,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.servlet.ServletContext;
 import vn.capigiba.laptopshop.domain.User;
 import vn.capigiba.laptopshop.service.UserService;
 
@@ -22,9 +28,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class UserController {
 
     private UserService userService;
+    private final ServletContext servletContext;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, ServletContext servletContext) {
         this.userService = userService;
+        this.servletContext = servletContext;
     }
 
     @RequestMapping("/")
@@ -54,15 +62,35 @@ public class UserController {
         return "admin/user/detail";
     }
 
-    @RequestMapping("/admin/user/create")
+    @GetMapping("/admin/user/create")
     public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
 
-    @RequestMapping(value = "admin/user/create", method = RequestMethod.POST)
-    public String createUserPage(Model model, @ModelAttribute("newUser") User capigiba) {
-        this.userService.handleSaveUser(capigiba);
+    @PostMapping(value = "admin/user/create")
+    public String createUserPage(Model model, @ModelAttribute("newUser") User capigiba,
+            @RequestParam("capiFile") MultipartFile file) {
+        try {
+            byte[] bytes;
+            bytes = file.getBytes();
+
+            String rootPath = this.servletContext.getRealPath("/resources/images");
+            File dir = new File(rootPath + File.separator + "avatar");
+            if (!dir.exists())
+                dir.mkdirs();
+            // Create the file on server
+            File serverFile = new File(dir.getAbsolutePath() + File.separator +
+                    +System.currentTimeMillis() + "-" + file.getOriginalFilename());
+            BufferedOutputStream stream = new BufferedOutputStream(
+                    new FileOutputStream(serverFile));
+            stream.write(bytes);
+            stream.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        // this.userService.handleSaveUser(capigiba);
         return "redirect:/admin/user";
     }
 
